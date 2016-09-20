6b26c19d004597634938c623554fc6f67f94df3c
unknown <madsystem@hotmail.com>
2016-09-20 22:51:00 +0200

    initial commit, pipe server added, injection module added

---
+++ Fragger/Fragger/PipeServer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fragger
+{
+    class PipeServer
+    {
+        public static void Start()
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                var server = new NamedPipeServerStream("FRAGGER_PIPE");
+                server.WaitForConnection();
+                StreamReader reader = new StreamReader(server);
+                StreamWriter writer = new StreamWriter(server);
+                while (true)
+                {
+                    var line = reader.ReadLine();
+                    writer.WriteLine(String.Join("", line.Reverse()));
+                    writer.Flush();
+                }
+            });
+
+        }
+    }
+}
